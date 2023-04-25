cd9925c347b04d16729ee3f06b8591d088595d29
Matthew <matthew.hansen9@gmail.com>
2023-04-25 15:17:55 -0400

    Update cp.js

--- cp.js
+++ cp.js
@@ -1,20 +1,20 @@
-$(".copy-button").each (function () { // for each copy button
-    $(this).on("click", function () {  
-        let codeSnippet = JSON.parse($(this).parent().find(".snippet").text()); // get the code snippet from the parent div
-        console.log("codeSnippet", codeSnippet); // log the code snippet
-        let copyButton = $(this); // get the copy button
-        let data = JSON.stringify(codeSnippet); // stringify the code snippet   
-        console.log("data", data); // log the data
-        document.addEventListener("copy", (event) => { // add the event listener
-        event.clipboardData.setData("application/json", data); // set the data to be copied to the clipboard
-        event.preventDefault(); // prevent the default copy behavior
-        console.log("copied to cb", data);  // log the data
-        copyButton.text("Copied to clipboard"); // change the button text
-        setTimeout(() => {
-            copyButton.text("Copy"); // change the button text back to the original
+$(".copy-button").each(function() {
+    $(this).on("click", function() {
+      let codeSnippet = JSON.parse($(this).parent().find(".snippet").text());
+      console.log("codeSnippet", codeSnippet);
+      let copyButton = $(this);
+      let data = JSON.stringify(codeSnippet);
+      console.log("data", data);
+      document.addEventListener("copy", function(event) {
+        event.clipboardData.setData("application/json", data);
+        event.preventDefault();
+        console.log("copied to cb", data);
+        copyButton.text("Copied to clipboard");
+        setTimeout(function() {
+          copyButton.text("Copy");
+          window.getSelection().removeAllRanges(); // clear the selection
         }, 3000);
-        });
-        document.execCommand("copy"); // copy the data to the clipboard
-    }
-    );
-});
+      });
+      document.execCommand("copy");
+    });
+  });
