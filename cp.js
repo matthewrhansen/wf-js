7507f4de73fbd6816db771a0c80e925e29326437
Matthew <matthew.hansen9@gmail.com>
2023-04-25 16:30:54 -0400

    Update cp.js

--- cp.js
+++ cp.js
@@ -1,27 +1,23 @@
-$(".copy-button").each(function() { // for each copy button
-  $(this).on("click", function() {
-    let codeSnippet = JSON.parse($(this).parent().find(".snippet").text()); // get the code snippet from the parent div
-    console.log("codeSnippet", codeSnippet); // log the code snippet
-    let copyButton = $(this); // get the copy button
-    let data = JSON.stringify(codeSnippet); // stringify the code snippet
-    console.log("data", data); // log the data
-    document.addEventListener("copy", (event) => { // add the event listener
-      event.clipboardData.setData("application/json", data); // set the data to be copied to the clipboard
-      event.preventDefault(); // prevent the default copy behavior
-      console.log("copied to cb", data); // log the data
-      
-      // Add a class to the most recently clicked button and remove it from all other buttons
-      $(".copy-button").removeClass("recently-clicked");
-      copyButton.addClass("recently-clicked");
-      
-      copyButton.text("Copied to clipboard"); // change the button text
-      setTimeout(() => {
-        // Only change the text back to the original if it is the most recently clicked button
-        if (copyButton.hasClass("recently-clicked")) {
-          copyButton.text("Copy");
-        }
-      }, 3000);
-    });
-    document.execCommand("copy"); // copy the data to the clipboard
-  });
+// Attach click event listener to parent element and delegate to ".copy-button"
+$(document).on("click", ".copy-button", function() {
+  let codeSnippet = JSON.parse($(this).parent().find(".snippet").text()); // get the code snippet from the parent div
+  console.log("codeSnippet", codeSnippet); // log the code snippet
+  let copyButton = $(this); // get the copy button
+  let data = JSON.stringify(codeSnippet); // stringify the code snippet
+  console.log("data", data); // log the data
+  copyToClipboard(data, copyButton); // call function to copy data to clipboard and change button text
 });
+
+// Function to copy data to clipboard and change button text
+function copyToClipboard(data, copyButton) {
+  document.addEventListener("copy", (event) => { // add the event listener
+    event.clipboardData.setData("application/json", data); // set the data to be copied to the clipboard
+    event.preventDefault(); // prevent the default copy behavior
+    console.log("copied to cb", data); // log the data
+    copyButton.text("Copied to clipboard"); // change the button text
+    setTimeout(() => {
+      copyButton.text("Copy"); // change the button text back to the original
+    }, 3000);
+  });
+  document.execCommand("copy"); // copy the data to the clipboard
+}
