f6e71d8bb2b952ff6462fe83a117a17ba7805069
Matthew <matthew.hansen9@gmail.com>
2023-04-25 15:53:43 -0400

    Update cp.js

--- cp.js
+++ cp.js
@@ -1,23 +1,23 @@
-$(".copy-button").each(function() {
-  $(this).on("click", function() {
-    let codeSnippet = JSON.parse($(this).parent().find(".snippet").text());
-    console.log("codeSnippet", codeSnippet);
-    let copyButton = $(this);
-    let data = JSON.stringify(codeSnippet);
-    console.log("data", data);
-    document.addEventListener("copy", function(event) {
-      event.clipboardData.setData("application/json", data);
-      event.preventDefault();
-      console.log("copied to cb", data);
-      copyButton.text("Copied to clipboard");
-      setTimeout(function() {
-        copyButton.text("Copy");
-        copyButton.off("mouseleave"); // remove the mouseleave event listener
-      }, 3000);
-    });
-    document.execCommand("copy");
-    $(".copy-button").not(copyButton).on("mouseleave", function() {
-      $(this).text("Copy"); // reset the text of all other copy buttons
-    });
+function copyToClipboard(button) {
+  let codeSnippet = JSON.parse(button.closest('.code-snippet').querySelector('.snippet').textContent.trim());
+  let data = JSON.stringify(codeSnippet);
+  console.log("data", data);
+  
+  navigator.clipboard.writeText(data).then(function() {
+    console.log("copied to clipboard", data);
+    button.textContent = "Copied!";
+    setTimeout(function() {
+      button.textContent = "Copy";
+    }, 3000);
+  }, function(error) {
+    console.error("copy failed", error);
   });
+}
+
+$(".copy-button").on("click", function() {
+  copyToClipboard(this);
+});
+
+$(".copy-button").on("mouseleave", function() {
+  $(this).text("Copy");
 });
