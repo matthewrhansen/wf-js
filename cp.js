5018139f759512bf26d1526d72783484e529435e
Matthew <matthew.hansen9@gmail.com>
2023-04-25 21:15:30 -0400

    Update cp.js

--- cp.js
+++ cp.js
@@ -1,3 +1,5 @@
+let lastClickedButton; // create a variable to keep track of the last clicked button
+
 $(document).on("click", ".copy-button", function() {
   let codeSnippet = JSON.parse($(this).parent().find(".snippet").text());
   console.log("codeSnippet", codeSnippet);
@@ -5,13 +7,16 @@ $(document).on("click", ".copy-button", function() {
   let data = JSON.stringify(codeSnippet);
   console.log("data", data);
   
+  // Only change text of last clicked button
+  if (lastClickedButton) {
+    lastClickedButton.text("Copy");
+  }
+  lastClickedButton = copyButton;
+  
   copyToClipboard(data, copyButton);
 });
 
 function copyToClipboard(data, copyButton) {
-  // Reset text of all buttons to "Copy"
-  $(".copy-button").text("Copy");
-  
   document.addEventListener("copy", (event) => {
     event.clipboardData.setData("application/json", data);
     event.preventDefault();
